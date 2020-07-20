6d415fe9be01cee2e74af611017af57c384ad16a
Martin Molinares Araujo <memolinares@uninorte.edu.co>
2020-07-20 18:43:15 -0500

    feat: form style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import Header from './components/Header';
+import Form from './components/Form';
+
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+`;
+
+const FormContainer = styled.div`
+  background-color: #FFFFFF;
+  padding: 3rem;
+`;
 
 function App() {
   return (
-    <Header 
-      titulo="Cotizador de Seguros" 
-    />
+    <Container>
+      <Header 
+        titulo="Cotizador de Seguros" 
+      />
+      <FormContainer>
+        <Form />
+      </FormContainer>
+    </Container>
+
   );
 }
 
